a0f87a878b821f08f9cc3d63261395513f2f4ccb
alnnasr <ia1@hotmail.de>
2015-07-07 13:40:08 +0000

    Create qa-lang-users.php

---
+++ ar/qa-lang-users.php
@@ -0,0 +1,151 @@
+<?php
+	
+/*
+	Question2Answer by Gideon Greenspan and contributors
+	http://www.question2answer.org/
+
+	File: qa-include/qa-lang-users.php
+	Description: Language phrases for user management
+
+
+	This program is free software; you can redistribute it and/or
+	modify it under the terms of the GNU General Public License
+	as published by the Free Software Foundation; either version 2
+	of the License, or (at your option) any later version.
+
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+
+	More about this license: http://www.question2answer.org/license.php
+*/
+
+	return array(
+		'about' => 'نبذة شخصية',
+		'add_user_x_favorites' => 'اضف المستخدم ^ إلى المفضلة',
+		'avatar_default' => 'افتراضي',
+		'avatar_gravatar' => 'اظهر ^1صورتي^2',
+		'avatar_label' => 'صورة شخصية:',
+		'avatar_none' => 'لا يوجد',
+		'avatar_gravatar' => 'اعرض ^1صورتي التعبيرية^2',
+#		'avatar_not_read' => 'لا يمكن قراءة الصورة، من فضلك ارفع واحدة من: ^',
+		'block_user_button' => 'حظر المستخدم',
+		'blocked_users' => 'المستخدمون المحظورون',
+		'change_email_link' => ' - ^1غير البريد^2',
+		'change_password' => 'تغيير كلمة المرور',
+		'confirm_complete' => 'شكرًا لك، تم تأكيد بريدك الإلكتروني.',
+		'confirm_emailed' => 'تم إرسال رابط تفعيل لبريدك الإلكتروني، من فضلك قم بالضغط على الرابط لتفعيل بريدك.',
+		'confirm_required' => 'لاستكمال التسجيل، يرجى النقر على رابط التأكيد الذي تم إرساله إلى بريدك الإلكتروني،أو ^1اطلب مرة أخرى	^2.',
+		'confirm_title' => 'تفعيل البريد الإلكتروني',
+		'confirm_wrong_log_in' => 'الكود ليس صحيحًا، من فضلك ^1سجل دخولك^2 لتحصل على رابط جديد',
+		'confirm_wrong_resend' => 'الكود ليس صحيحًا، من فضلك أنقر بالأسفل لتحصل على رابط جديد',
+		'delete_user_button' => 'حذف مستخدم',
+		'edit_profile' => 'تعديل ملفي الشخصي',
+#		'edit_level_button' => 'عدل مستوى المستخدم',
+		'edit_user_button' => 'عدل هذا المستخدم',
+		'email_confirmed' => 'تم التأكيد',
+		'email_exists' => 'البريد الإلكتروني مستخدم بالفعل',
+		'email_handle_label' => 'البريد الإلكتروني، أو اسم المستخدم:',
+		'email_invalid' => 'أدخل عنوان بريد إلكتروني صالح من فضلك.',
+		'email_label' => 'البريد الإلكتروني:',
+		'email_not_confirmed' => 'لم يتم التأكيد',
+		'email_please_confirm' => 'من فضلك ^5قم بالتأكيد^6',
+		'email_required' => 'البريد الإلكتروني مطلوب، لن يعرض للعموم',
+		'forgot_link' => 'نسيت كلمة المرور؟',
+		'full_name' => 'الاسم الكامل',
+		'handle_empty' => 'لا يمكن ترك خانة اسم المستخدم فارغًا',
+		'handle_exists' => 'اسم المستخدم محجوز، من فضلك جرب اسمًا آخر',
+		'handle_has_bad' => 'لا يمكن لاسم المستخدم أن يحتوي على: ^',
+		'handle_label' => 'اسم المستخدم:',
+		'hide_all_user_button' => 'اخف جميع مشاركات هذا المستخدم	',
+		'last_login_label' => 'تسجيل الدخول الأخير:',
+		'last_write_label' => 'آخر إدخال:',
+		'level_admin' => 'مدير',
+		'level_editor' => 'محرر',
+		'level_expert' => 'خبير',
+		'level_moderator' => 'وسيط',
+		'level_super' => 'مدير أعلى',
+		'location' => 'المكان',
+		'log_in_to_access' => 'يمكنك الآن ^1تسجيل الدخول^2 لتصل إلى حسابك.',
+		'login_button' => 'تسجيل الدخول',
+		'login_limit' => 'لقد تجاوزت الحد الأعلى لمحاولات تسجيل الدخول، من فضلك عُد بعد فترة',
+		'login_title' => 'دخول',
+		'mass_mailings_explanation' => 'الاشتراك في الرسائل البريدية لجميع المستخدمين',
+		'mass_mailings' => 'الرسائل الكمية:',
+		'member_for' => 'مستخدم منذ:',
+		'member_type' => 'الرتبة:',
+		'new_password_1' => 'كلمة المرور الجديدة:',
+		'new_password_2' => 'أعد إدخال كلمة المرور الجديدة:',
+		'no_blocked_users' => 'لم يتم إيجاد مستخدمين محظورين',
+		'no_permission' => 'لا تمتلك الصلاحية للقيام بهذه العملية',
+		'old_password' => 'كلمة المرور القديمة',
+		'only_shown_admins' => '[فقط المدراء يمكنهم الاطلاع عليه]',
+		'only_shown_moderators' => '[فقط الوسطاء يمكنهم الاطلاع عليه]',
+		'password_changed' => 'تم تغيير كلمة المرور',
+		'password_label' => 'كلمة المرور:',
+		'password_min' => 'كلمة المرور ينبغي أن تكون أكثر من ^ حرفًا',
+		'password_mismatch' => 'كلمتا المرور غير متطابقتان',
+		'password_none' => 'لا يوجد. لتسجل دخولك مباشرة، عين كلمة مرور بالأسفل.',
+		'password_sent' => 'تم إرسال كلمة المرور الجديدة إليك على بريدك الإلكتروني',
+		'password_to_set' => 'يرجى إعدادها على صفحة حسابك',
+		'password_wrong' => 'كلمة المرور غير صحيحة',
+		'private_messages_explanation' => 'اسمح للمستخدمين بمراسلتي [دون إظهار عنوان بريدي الإلكتروني]',
+		'private_messages' => 'رسائل خاصة:',
+		'profile_saved' => 'تم حفظ الملف الشخصي',
+		'register_button' =>	 'تسجيل',
+		'register_limit' => 'كثرت التسجيلات - الرجاء المحاولة بعد ساعة',
+		'register_suspended' => 'تسجيل المستخدمين الجدد تم تعليقه مؤقتاً. الرجاء المحاولة لاحقاً.',
+		'register_title' => 'تسجيل كمستخدم جديد',
+		'registered_user' => 'مستخدم مسجل',
+		'remember_label' => 'تذكرني على هذا الحاسوب',
+		'remove_avatar' => 'ازل الصورة الشخصية:',
+		'reset_code_another' => 'أرسل آخر',
+		'reset_code_emailed' => 'تم إرسال كود إعادة التعيين إليك على بريدك الإلكتروني',
+		'reset_code_label' => 'الكود:',
+		'reset_code_wrong' => 'الكود غير صحيح',
+		'reset_title' => 'إعادة تعيين كلمة المرور المنسية',
+		'save_profile' => 'حفظ الملف الشخصي',
+		'save_user' => 'حفظ المستخدم',
+		'send_confirm_button' => 'أرسل رابط التقعيل',
+		'send_password_button' => 'أرسل كلمة المرور الجديدة',
+		'send_reset_button' => 'أرسل البريد الإلكتروني لإعادة التفعيل',
+		'send_reset_note' => 'سيتم إرسال رسالة إلى بريدك الإلكتروني بالتعليمات.',
+		'special_users' => 'مستخدمون خاصون',
+		'terms_not_accepted' => 'يجب عليك وضع علامة هنا',
+		'unblock_user_button' => 'مستخدم غير محظور',
+		'user_blocked' => '(محظور)',
+		'user_not_found' => 'لم يتم إيجاد المستخدم',
+#		'view_account_page' => 'عد إلى ^1صفحة^2 حسابك.',
+		'website' => 'الموفع',
+		'unsubscribe' => 'إلغاء الاشتراك:',
+		'unsubscribe_complete' => 'لقد تم إلغاء اشتراكك من البريد الكمي المرسل إليك من ^0. يمكنك إعادة الاشتراك متى شئت عبر زيارة  ^1 صفحة حسابك^2.', 
+		'unsubscribe_title' => 'إلغاء الاشتراك',
+		'unsubscribe_wrong_log_in' => 'لم يتم التصحيح - يرجى ^1الدخول ^2 لإلغاء الاشتراك',
+		'x_ago_from_y' => 'منذ ^1 من ^2',
+
+		'approve_required' => "يرجى انتظار الموافقة على الحساب أو ^1قم بإضافة معلومات أكثر^2.",
+		'approve_title' => "في انتظار الموافقة على المستخدم",
+		'approve_user_button' => "اعتماد المستخدم",
+		'approved_user' => "المستخدم معتمد",
+		'category_level_add' => " - ^1إضافة وسم بصلاحيات معينة^2",
+		'category_level_in' => "للوسم التالي:",
+		'category_level_label' => "تم الترقية إلى:",
+		'category_level_none' => "لا ترقية",
+		'level_for_category' => "^1 لأجل ^2",
+		'level_in_general' => "بشكل عام",
+		'only_shown_editors' => "(يستطيع المحررون وما فوقهم مشاهدتها)",
+		'only_shown_experts' => "(يستطيع الخبراء وما فوقهم مشاهدتها)",
+		'wall_posts' => "مشاركات الحائط:",
+		'wall_posts_explanation' => "السماح للمستخدمين بالمشاركة على حائطك (سيتم مراسلتك أيضاً)",
+		'registered_label' => "مسجل:",
+		'remember' => "تذكر",
+
+
+
+		);
+	
+
+/*
+	Omit PHP closing tag to help avoid accidental output
+*/
