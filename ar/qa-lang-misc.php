1474d21a9d18af84ded1b4249d390b6fb579c4a5
alnnasr <ia1@hotmail.de>
2015-07-07 13:35:18 +0000

    Create qa-lang-misc.php

---
+++ ar/qa-lang-misc.php
@@ -0,0 +1,127 @@
+<?php
+	
+/*
+	Question2Answer by Gideon Greenspan and contributors
+	http://www.question2answer.org/
+
+	File: qa-include/qa-lang-misc.php
+	Description: Miscellaneous language phrases
+
+
+	This program is free software; you can redistribute it and/or
+	modify it under the terms of the GNU General Public License
+	as published by the Free Software Foundation; either version 2
+	of the License, or (at your option) any later version.
+
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+
+	More about this license: http://www.question2answer.org/license.php
+*/
+
+	return array(
+		'block_ip_button' => 'حظر عنوان IP',
+		'browse_categories' => 'استعراض التصنيفات',
+		'captcha_approve_fix' => 'سيتوقف مانع الإزعاج هذا بمجرد اعتماد حسابك.',
+		'captcha_confirm_fix' => 'لتتجنب هذا التأكيد في المستقبل، من فضلك ^5أكد عنوان بريدك الإلكتروني^6.',
+		'captcha_error' => 'أكمل تأكيد مانع الإزعاج من فضلك',
+		'captcha_label' => 'تأكيد مانع الإزعاج:',
+		'captcha_login_fix' => 'لتتجنب هذا التأكيد في المستقبل، من فضلك ^1سجل دخولك^2 or ^3أو قم بإنشاء حساب جديد^4.',
+		'feed_a_edited_prefix' => 'تم تعديل الإجابة عليه: ',
+		'feed_a_prefix' => 'تم الإجابة عليه: ',
+		'feed_a_reshown_prefix' => 'تم إظهار الإجابة مرة أخرى: ',
+		'feed_a_selected_prefix' => 'تم اختيار الإجابة: ',
+		'feed_c_edited_prefix' => 'تم تعديل التعليق: ',
+		'feed_c_moved_prefix' => 'تم نقل التعليق: ',
+		'feed_c_prefix' => 'تم التعليق عليه: ',
+		'feed_c_reshown_prefix' => 'تم إظهار التعليق مرة أخرى: ',
+		'feed_closed_prefix' => 'مقفل: ',
+		'feed_edited_prefix' => 'تم تعديله: ',
+		'feed_hidden_prefix' => 'مخفي: ',
+		'feed_not_found' => 'لم يتم إيجاد التغذية',
+		'feed_recategorized_prefix' => 'تم إعادة التصنيف: ',
+		'feed_reopened_prefix' => 'تم الفتح مرة أخرى: ',
+		'feed_reshown_prefix' => 'تم الإظهار مرة أخرى: ',
+		'feed_retagged_prefix' => 'تم إعادة تسمية الوسم: ',
+		'feedback_email' => 'بريدك الإلكتروني: (اختياري)',
+		'feedback_empty' => 'استخدم هذا الحقل لإرسال تعليقاتك أو مقترحاتك',
+		'feedback_message' => 'تعليقاتك أو مقترحاتك ل^:',
+		'feedback_name' => 'اسمك: (اختياري)',
+		'feedback_sent' => 'تم إرسال رسالتك بنجاح، شكرًا لك.',
+		'feedback_title' => 'اتصل بنا',
+		'form_security_again' => 'يرجى النقر مرة أخرى للتأكيد',
+		'form_security_reload' => 'يرجى إعادة تحميل الصفحة ثم المحاولة مرة أخرى',
+		'hide_all_ip_button' => 'إخفاء جميع المشاركات من عنوان IP هذا',
+		'host_name' => 'اسم المضيف:',
+		'inbox' => 'البريد الوارد',
+		'matches_blocked_ips' => 'عناوين IP المحظورة المتطابقة:',
+		'message_empty' => 'فضلاً، أكتب رسالتك المراد إبلاغها لهذا المستخدم',
+		'message_explanation' => 'سوف يتم الإرسال كإعلام من ^. لن يتم الكشف عن بريدك مالم تقم أنت بتضمينه في الرسالة.',
+		'message_for_x' => 'رسالتك لـ^:',
+		'message_limit' => 'لقد تم إرسال العديد من الرسائل الخاصة - الرجاء المحاولة بعد ساعة',
+		'message_must_login' => 'فضلاً، ^1سجل الدخول^2 أو ^3اشترك بالتسجيل^4 لإرسال رسائل خاصة.',
+#		'message_received_x_ago' => 'تم استلامه منذ ^:',
+		'message_recent_history' => 'التوافق الحالي مع ^',
+#		'message_sent_x_ago' => 'تم الإرسال منذ ^:',
+		'message_sent' => 'لقد تم إرسال رسالتك بالأسفل',
+		'more_favorite_qs' => '...المزيد من الأسئلة المفضلة',
+		'more_favorite_tags' => '...المزيد من الوسوم المفضلة',
+		'more_favorite_users' => "...المزيد من المستخدمين المفضلين",
+		'my_favorites_title' => 'المفضلة',
+		'nav_all_my_updates' => 'جميع تحديثاتي',
+		'nav_my_content' => 'محتواي',
+		'nav_my_details' => 'تفاصيلي',
+		'nav_my_favorites' => 'المفضلة',
+		'nav_user_activity' => 'آخر الأنشطة',
+		'nav_user_as' => 'الإجابات',
+		'nav_user_pms' => 'رسائل خاصة',
+		'nav_user_qs' => 'الأسئلة',
+		'nav_user_wall' => 'ملصق',
+		'no_activity_from_x' => 'لا يوجد نشاط لـ ^',
+		'no_favorite_categories' => 'لا توجد تصنيفات مفضلة',
+		'no_favorite_qs' => 'لا توجد أسئلة مفضلة',
+		'no_favorite_tags' => 'لا توجد وسوم مفضلة',
+		'no_favorite_users' => 'لا يوجد مستخدمون مفضلون',
+		'no_recent_updates' => 'لا توجد تحديثات حالية',
+		'no_updates_content' => 'لا توجد تحديثات حالية لمحتواي',
+		'no_updates_favorites' => 'لا توجد تحديثات لمفضلتي',
+		'outbox' => 'العناصر المرسلة',
+		'private_message_title' => 'إرسال رسالة خاصة',
+		'pm_inbox_title' => 'الرسائل الخاصة المستلمة',
+		'pm_outbox_title' => 'الرسائل الخاصة المرسلة',
+		'recent_activity_from_x' => 'آخر نشاط بواسطة ^',
+		'recent_updates_content' => 'تحديثات حالية لمحتواي',
+		'recent_updates_favorites' => 'تحديثات حالية لمفضلتي',
+		'recent_updates_title' => 'تحديثات حالية لي',
+		'site_in_maintenance' => 'هذا الموقع متوقف للصيانة، من فضلك عد في وقت آخر.',
+		'suggest_favorites_add' => 'لإضافة سؤال أو بند آخر لمفضلتك، انقر على ^ أعلى الصفحة الخاصة به.',
+		'suggest_update_favorites' => 'لتحديثات أخرى، أضف العناصر إلى ^1مفضلتك^2.',
+		'unblock_ip_button' => 'إلغاء حظر عنوان IP',
+		'your_a_commented' => 'تم التعليق على إجابتك',
+		'your_a_edited' => 'تم تعديل إجابتك',
+		'your_a_hidden' => 'تم إخفاء إجابتك',
+		'your_a_questioned' => 'طرح سؤال حول إجابتك',
+		'your_a_reshown' => 'تم إعادة إظهار إجابتك',
+		'your_a_selected' => 'تم اختيار إجابتك',
+		'your_c_edited' => 'تم تعديل تعليقك',
+		'your_c_followed' => 'تم متابعة تعليقك',
+		'your_c_hidden' => 'تم إخفاء تعليقك',
+		'your_c_moved' => 'تم نقل تعليقك',
+		'your_c_reshown' => 'تم إعادة إظهار تعليقك',
+		'your_q_answered' => 'تم إظهار سؤالك',
+		'your_q_closed' => 'تم إغلاق سؤالك',
+		'your_q_commented' => 'تم التعليق على سؤالك',
+		'your_q_edited' => 'تم تعديل سؤالك',
+		'your_q_hidden' => 'تم إخفاء سؤالك',
+		'your_q_recategorized' => 'تم إعادة تصنيف سؤالك',
+		'your_q_reopened' => 'تم إعادة فتح سؤالك',
+		'your_q_reshown' => 'تم إعادة إظهار سؤالك',
+		'your_q_retagged' => 'تم إعادة وسم سؤالك',
+	);
+	
+
+/*
+	Omit PHP closing tag to help avoid accidental output
+*/
