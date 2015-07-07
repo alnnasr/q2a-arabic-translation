b11767224f4d25f25ab5e556ee54449a96bc8b3a
alnnasr <ia1@hotmail.de>
2015-07-07 13:37:19 +0000

    Create qa-lang-profile.php

---
+++ ar/qa-lang-profile.php
@@ -0,0 +1,93 @@
+<?php
+	
+/*
+	Question2Answer by Gideon Greenspan and contributors
+	http://www.question2answer.org/
+
+	File: qa-include/qa-lang-profile.php
+	Description: Language phrases for user profile page
+
+
+	This program is free software; you can redistribute it and/or
+	modify it under the terms of the GNU General Public License
+	as published by the Free Software Foundation; either version 2
+	of the License, or (at your option) any later version.
+
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+
+	More about this license: http://www.question2answer.org/license.php
+*/
+
+	return array(
+		'1_chosen_as_best' => ' (1 مختار كالأفضل)',
+		'1_down_vote' => '1 تصويت بالسلب',
+		'1_up_vote' => '1 تصويت بالموجب',
+		'1_with_best_chosen' => ' (1 أفضل إجابة مختارة)',
+		'activity_by_x' => 'إحصاءات ^',
+		'answers' => 'الإجابات:',
+		'answers_by_x' => 'إجابات بواسطة ^',		
+		'bonus_points' => 'نقاط إضافية:',
+		'comments' => 'التعليقات:',
+		'delete_wall_post_popup' => 'حذف مشاركة الحائط هذه',
+		'delete_pm_popup' => 'حذف هذه الرسالة الخاصة',
+		'extra_privileges' => 'صلاحيات إضافية:',
+		'gave_out' => 'أعطى',
+		'my_account_title' => 'تفاصيل حسابي',
+		'no_answers_by_x' => 'لا إجابات من قبل ^',
+		'no_posts_by_x' => 'لا مشاركات من قبل ^',
+		'no_questions_by_x' => 'لا أسئلة من قبل ^',
+		'permit_anon_view_ips' => 'عرض عناوين IP لمشاركات المجهولين',
+		'permit_close_q' => 'إغلاق أي سؤال',
+		'permit_delete_hidden' => 'حذف المشاركات المخفية',
+		'permit_edit_a' => 'تحرير أي إجابة',
+		'permit_edit_c' => 'تحرير أي تعليق',
+		'permit_edit_q' => 'تحرير أي سؤال',
+		'permit_edit_silent' => 'تعديل المشاركات بصمت',
+		'permit_flag' => 'تعليم المشاركات',
+		'permit_hide_show' => 'إخفاء أو إظهار أي مشاركات',
+		'permit_moderate' => 'اعتماد أو رفض المشاركات',
+		'permit_post_a' => 'الرد على الأسئلة',
+		'permit_post_c' => 'إضافة تعليقات',
+		'permit_post_q' => 'طرح أسئلة',
+		'permit_post_wall' => 'المشاركة في ملصقات المستخدمين',
+		'permit_recat' => 'إعادة تصنيف أي سؤال',
+		'permit_retag' => 'إعادة وسم أي سؤال',
+		'permit_select_a' => 'اختيار إجابة لأي سؤال',
+		'permit_view_q_page' => 'عرض صفحات السؤال',
+		'permit_view_voters_flaggers' => 'عرض المصوتين والمعلمين عن المشاركات',
+		'permit_vote_a' => 'تقييم الإجابات',
+		'permit_vote_down' => 'تقييم المشاركات سلباً',
+		'permit_vote_q' => 'تقييم الأسئلة',
+		'post_wall_blocked' => 'لقد عطل هذا المستخدم إمكانية وضع مشاركات على ملصقه',
+		'post_wall_button' => 'إضافة مصلق حائط',
+		'post_wall_empty' => 'يرجى إدراج أي شيء لوضعه على ملصق الحائط هذا',
+		'post_wall_limit' => 'لا تستطيع كتابة ملصقات حائط إضافية هذه الساعة',
+		'post_wall_must_be_approved' => 'يجب ترخيص حسابك لتمكينك من المشاركة بملصقات الحائط هنا.',
+		'post_wall_must_confirm' => 'يرجى يا ^5تأكيد عنوان بريدك الإلكتروني ^6 للمشاركة في هذا ملصق الحائط.',
+		'post_wall_must_login' => 'يرجى يا ^1تسجيل الدخول^2 أو^3الاشتراك^4 للمشاركة في هذا ملصق الحائط.',
+		'questions' => 'الأسئلة:',
+		'questions_by_x' => 'أسئلة لـ ^',
+		'ranked_x' => ' (مصنف #^)',
+		'received' => 'الاستقبالات:',
+		'recent_activity_by_x' => 'آخر نشاط بواسطة ^',
+		'score' => 'النقاط:',
+		'send_private_message' => ' - ^1طرح رسالة خاصة إلى^2',
+		'set_bonus_button' => 'تحديث العلاوات',
+		'title' => 'العنوان:',
+		'user_x' => 'المستخدم ^',
+		'voted_on' => 'صوت لـ:',
+		'wall_for_x' => 'ملصق لـ ^',
+		'wall_view_more' => 'اظهر المزيد من ملصقات المشاركات...',
+		'x_chosen_as_best' => ' (^ مختار كالأفضل)',
+		'x_down_votes' => '^ تصويت بالسالب',
+		'x_up_votes' => '^ تصويت بالموجب',
+		'x_with_best_chosen' => ' (^ أفضل إجابة مختارة)',
+	);
+	
+
+/*
+	Omit PHP closing tag to help avoid accidental output
+*/
